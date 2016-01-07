3a810d384f1397f9e145f63a77a4723820bd505a
Stompf <anton.lundmark@gmail.com>
2016-01-07 23:48:59 +0100

    more work

--- AchtungTypescriptClient/AchtungTypescriptClient/Scripts/game/app.ts
+++ AchtungTypescriptClient/AchtungTypescriptClient/Scripts/game/app.ts
@@ -16,4 +16,7 @@ class AchtungTypescript {
 
 
     }
+
+
+
 } export = AchtungTypescript;
--- AchtungTypescriptServer/AchtungTypescriptServer/app.ts
+++ AchtungTypescriptServer/AchtungTypescriptServer/app.ts
@@ -2,19 +2,25 @@
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+import collection = require('./Scripts/collections');
+
 http.listen(3000, () => {
     console.log('listening on *:3000');
 });
 
+const connections = new collection.Dictionary<string, SocketIO.Socket>();
+
 io.on('connection', (socket: SocketIO.Socket) => {
-    console.log('a user connected');
+    connections.setValue(socket.id, socket);
+    console.log('a user connected with id: ' + socket.id);
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        connections.remove(socket.id);
+        console.log(socket.id + ' - user disconnected');
     });
 
     socket.on('chat message', (msg: string) => {
-        console.log('message: ' + msg);
+        console.log(socket.id + ' - message: ' + msg);
         io.emit('chat message', msg);
     });
 });
